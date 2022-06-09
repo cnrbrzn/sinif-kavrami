f1d9430127740b54844ce7a7570d280513007e7f
Caner Borazan <canerborazann@gmail.com>
2022-06-09 13:43:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Söz Dizimi
+// class SinifAdi
+// {
+//     [Erişim Belirleyici] [Veri Tipi] ÖzellikAdi;
+//     [Erişim Belirleyici] [Geri Dönüş Tipi] MetotAdi([Parametre Listesi])
+//     {
+//         Metot Komutları
+//     }
+// }
+
+//Erişim Belirleyiciler
+// * Public
+// * Private
+// * Interval --> sadece bulunduğu proje içerisinde
+// * Protected --> sadece tanımlandığı sınıfta veya o sınıftan kalıtım/miras alan diğer sınıflarda
+
+Calisan calisan1 = new Calisan();
+calisan1.Ad = "Ayşe";
+calisan1.Soyad = "Kara";
+calisan1.No = 23415634;
+calisan1.Departman = "İnsan Kaynakları";
+
+calisan1.CalisanBilgileri();
+
+Console.WriteLine("**********");
+
+Calisan calisan2 = new Calisan();
+calisan2.Ad = "Deniz";
+calisan2.Soyad = "Arda";
+calisan2.No = 25646789;
+calisan2.Departman = "Satın Alma";
+
+calisan2.CalisanBilgileri();
+
+class Calisan
+{
+    public string Ad;
+    public string Soyad;
+    public int No;
+    public string Departman;
+
+    public void CalisanBilgileri()
+    {
+        Console.WriteLine("Çalışanın Adı:{0}",Ad);
+        Console.WriteLine("Çalışanın Soyadı:{0}",Soyad);
+        Console.WriteLine("Çalışanın Numarası:{0}",No);
+        Console.WriteLine("Çalışanın Departmanı:{0}",Departman);
+    }
+}
